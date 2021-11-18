7c69b05c071aaf5d6099e551d7680e43a788c6dc
anand1987np <anand1987np@gmail.com>
2021-11-18 01:42:51 -0600

    changes for database

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -4,7 +4,7 @@ var baseURL;
 if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === "PRODUCTION") {
     baseURL = process.env.REACT_APP_API_BASE_URL;
 } else {
-    baseURL = "http://127.0.0.1:8000";
+    baseURL = "https://back-end-jb.herokuapp.com/";
 }
 
 const api = axios.create({
